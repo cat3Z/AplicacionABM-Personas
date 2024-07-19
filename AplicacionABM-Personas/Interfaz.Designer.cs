0a4a0379a4775f20562f79c2b5959c6291315d72
catscu <88331081+catscu@users.noreply.github.com>
2024-07-18 23:55:50 -0300

    Agregar archivos de proyecto.

---
+++ AplicacionABM-Personas/Interfaz.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace AplicacionABM_Personas
+{
+    partial class Interfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listaPersonas = new System.Windows.Forms.ListBox();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textApellido = new System.Windows.Forms.TextBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelApellido = new System.Windows.Forms.Label();
+            this.botonAlta = new System.Windows.Forms.Button();
+            this.botonBaja = new System.Windows.Forms.Button();
+            this.botonModificar = new System.Windows.Forms.Button();
+            this.botonBuscar = new System.Windows.Forms.Button();
+            this.textBuscar = new System.Windows.Forms.TextBox();
+            this.labelBuscar = new System.Windows.Forms.Label();
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listaPersonas
+            // 
+            this.listaPersonas.FormattingEnabled = true;
+            this.listaPersonas.Location = new System.Drawing.Point(331, 86);
+            this.listaPersonas.Name = "listaPersonas";
+            this.listaPersonas.Size = new System.Drawing.Size(352, 251);
+            this.listaPersonas.TabIndex = 0;
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(136, 86);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(155, 20);
+            this.textId.TabIndex = 1;
+            // 
+            // textApellido
+            // 
+            this.textApellido.Location = new System.Drawing.Point(136, 138);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(155, 20);
+            this.textApellido.TabIndex = 2;
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(136, 112);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(155, 20);
+            this.textNombre.TabIndex = 3;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(55, 89);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(18, 13);
+            this.labelId.TabIndex = 4;
+            this.labelId.Text = "ID";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(55, 115);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(44, 13);
+            this.labelNombre.TabIndex = 5;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelApellido
+            // 
+            this.labelApellido.AutoSize = true;
+            this.labelApellido.Location = new System.Drawing.Point(55, 141);
+            this.labelApellido.Name = "labelApellido";
+            this.labelApellido.Size = new System.Drawing.Size(44, 13);
+            this.labelApellido.TabIndex = 6;
+            this.labelApellido.Text = "Apellido";
+            // 
+            // botonAlta
+            // 
+            this.botonAlta.Location = new System.Drawing.Point(52, 173);
+            this.botonAlta.Name = "botonAlta";
+            this.botonAlta.Size = new System.Drawing.Size(76, 37);
+            this.botonAlta.TabIndex = 7;
+            this.botonAlta.Text = "Alta";
+            this.botonAlta.UseVisualStyleBackColor = true;
+            // 
+            // botonBaja
+            // 
+            this.botonBaja.Location = new System.Drawing.Point(134, 173);
+            this.botonBaja.Name = "botonBaja";
+            this.botonBaja.Size = new System.Drawing.Size(76, 37);
+            this.botonBaja.TabIndex = 8;
+            this.botonBaja.Text = "Baja";
+            this.botonBaja.UseVisualStyleBackColor = true;
+            this.botonBaja.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // botonModificar
+            // 
+            this.botonModificar.Location = new System.Drawing.Point(216, 173);
+            this.botonModificar.Name = "botonModificar";
+            this.botonModificar.Size = new System.Drawing.Size(76, 37);
+            this.botonModificar.TabIndex = 9;
+            this.botonModificar.Text = "Modificar";
+            this.botonModificar.UseVisualStyleBackColor = true;
+            this.botonModificar.Click += new System.EventHandler(this.botonModificar_Click);
+            // 
+            // botonBuscar
+            // 
+            this.botonBuscar.Location = new System.Drawing.Point(216, 314);
+            this.botonBuscar.Name = "botonBuscar";
+            this.botonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.botonBuscar.TabIndex = 10;
+            this.botonBuscar.Text = "Buscar";
+            this.botonBuscar.UseVisualStyleBackColor = true;
+            // 
+            // textBuscar
+            // 
+            this.textBuscar.Location = new System.Drawing.Point(52, 290);
+            this.textBuscar.Name = "textBuscar";
+            this.textBuscar.Size = new System.Drawing.Size(199, 20);
+            this.textBuscar.TabIndex = 11;
+            // 
+            // labelBuscar
+            // 
+            this.labelBuscar.AutoSize = true;
+            this.labelBuscar.Location = new System.Drawing.Point(55, 264);
+            this.labelBuscar.Name = "labelBuscar";
+            this.labelBuscar.Size = new System.Drawing.Size(52, 13);
+            this.labelBuscar.TabIndex = 12;
+            this.labelBuscar.Text = "Buscador";
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitulo.Location = new System.Drawing.Point(185, 27);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(369, 21);
+            this.labelTitulo.TabIndex = 13;
+            this.labelTitulo.Text = "Alta, Baja y Modificación de Usuarios";
+            this.labelTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Interfaz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTitulo);
+            this.Controls.Add(this.labelBuscar);
+            this.Controls.Add(this.textBuscar);
+            this.Controls.Add(this.botonBuscar);
+            this.Controls.Add(this.botonModificar);
+            this.Controls.Add(this.botonBaja);
+            this.Controls.Add(this.botonAlta);
+            this.Controls.Add(this.labelApellido);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.textApellido);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.listaPersonas);
+            this.Name = "Interfaz";
+            this.Text = "Interfaz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listaPersonas;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelApellido;
+        private System.Windows.Forms.Button botonAlta;
+        private System.Windows.Forms.Button botonBaja;
+        private System.Windows.Forms.Button botonModificar;
+        private System.Windows.Forms.Button botonBuscar;
+        private System.Windows.Forms.TextBox textBuscar;
+        private System.Windows.Forms.Label labelBuscar;
+        private System.Windows.Forms.Label labelTitulo;
+    }
+}
---
+++ Modelos/Modelo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Modelos
+{
+    public abstract class Modelo
+    {
+        public string IP;
+        public string NombreBase;
+        public string NombreDeUsuario;
+        public string Contrasenia;
+
+        public MySqlConnection Conexion;
+        public MySqlCommand Comando;
+        public MySqlDataReader Lector;
+
+        public Modelo()
+        {
+            this.IP = "127.0.0.10";
+            this.NombreBase = "DB-AplicacionABM";
+            this.Contrasenia = "";
+            this.NombreDeUsuario = "root";
+
+            this.Conexion = new MySqlConnection(
+                $"server={this.IP};" +
+                $"user={this.NombreDeUsuario};" +
+                $"password={this.Contrasenia};" +
+                $"database={this.NombreBase};"
+            );
+
+            this.Conexion.Open();
+
+            this.Comando = new MySqlCommand();
+            this.Comando.Connection = this.Conexion;
+
+        }
+
+
+    }
+}
